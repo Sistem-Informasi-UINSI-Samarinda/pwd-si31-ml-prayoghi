18093bac7989006ccdde19f2510073f24ab4f0f0
ml-prayoghi <prayoghi@uinsi.ac.id>
2025-11-12 15:40:17 +0800

    tes

---
+++ config/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "pwd_dosen";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+    die("Koneksi Gagal: ". mysqli_connect_error());
+}
+
+echo "Koneksi Berhasil";
+mysqli_close($conn);
+
+?>
